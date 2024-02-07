13f0e762e0e2eec866d4511c6ed9ca16393add07
ldgomm <ruiz211@protonmail.com>
2024-02-07 13:13:34 -0500

    Project initilisation

---
+++ Maia/MaiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaiaApp.swift
+//  Maia
+//
+//  Created by José Ruiz on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MaiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
